fe25ff4281d2e66734b93afb91110b678242d790
Prateet Tiwari <prateettiwari29@gmail.com>
2025-10-15 15:13:28 +0530

    Add routing with react-router-dom and create AuthPage, Home, NotFound pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AuthPage from "./pages/AuthPage";
+import NotFound from "./pages/NotFound";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<AuthPage />}></Route>
+      </Routes>
+    </>
+  );
 };
 
 export default App;
